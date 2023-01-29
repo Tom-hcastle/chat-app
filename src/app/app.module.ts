4573e2bf59a31d706e5ef8df1ba9709bab80d1dd
tom <19705445@students.lincoln.ac.uk>
2023-01-29 12:13:38 +0000

    FireChat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCaTi2Ak4A1IG8gX_NShDNJEbwvMCwQLk0",
+    authDomain: "firechat-cb7e0.firebaseapp.com",
+    databaseURL: "https://firechat-cb7e0-default-rtdb.firebaseio.com",
+    projectId: "firechat-cb7e0",
+    storageBucket: "firechat-cb7e0.appspot.com",
+    messagingSenderId: "397943679091",
+    appId: "1:397943679091:web:d06fb535819229e8ba2206",
+    measurementId: "G-1Q3JDVWMC7"
+  },
+  production: false
+};
+
